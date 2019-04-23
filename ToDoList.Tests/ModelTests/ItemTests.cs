492adb8b9b06cdac18af50a493a32e3ff0df0a48
dylan crocker and Nadia Lizcano <nadia@>
2019-04-23 15:41:09 -0700

    Add unit testing

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -1,11 +1,71 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System;
 using ToDoList.Models;
 
 namespace ToDoList.Tests
 {
     [TestClass]
-    public class ItemTest
+    public class ItemTest : IDisposable
     {
+        public void Dispose()
+        {
+            Item.ClearAll();
+        }
+        
+        [TestMethod]
+        public void ItemConstructor_CreatesInstanceOfItem_Item()
+        {
+            Item newItem = new Item("test");
+            Assert.AreEqual(typeof(Item), newItem.GetType());
+        }
+
+        [TestMethod]
+        public void GetDescription_ReturnDescription_String()
+        {
+            string description = "Walk the dog.";
+            Item newItem = new Item(description);
+            string result = newItem.GetDescription();
+            Assert.AreEqual(description, result);
+
+        }
+
+        [TestMethod]
+        public void SetDescription_SetDescription_String()
+        {
+            string description = "Walk the dog.";
+            Item newItem = new Item(description);
+            
+            string updatedDescription = "Do the dishes";
+            newItem.SetDescription(updatedDescription);
+            string result = newItem.GetDescription();
+
+            Assert.AreEqual(updatedDescription, result);        
+        }
+
+        [TestMethod]
+        public void GetAll_ReturnsAllItems_ItemList()
+        {
+            string description01 = "Walk the dog.";
+            string description02 = "Wash the dishes.";
+            Item newItem1 = new Item(description01);
+            Item newItem2 = new Item(description02);
+            
+            // Arrange
+            List<Item> newList = new List<Item> { newItem1, newItem2 };
+
+            // Act
+            List<Item> result = Item.GetAll();
+
+            foreach (Item thisItem in result)
+            {
+                Console.WriteLine("Output from GetAll test: " + thisItem.GetDescription());
+            }
+
+            CollectionAssert.AreEqual(newList, result);
+        }
+
+
 
     }
 }
--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,7 +1,42 @@
+using System.Collections.Generic;
+
 namespace ToDoList.Models
 {
     public class Item
     {
-        
+        //Properties
+        private string _description;
+
+        private static List<Item> _instances = new List<Item> {};
+
+
+        //Constructor
+        public Item (string description)
+        {
+            _description = description;
+            _instances.Add(this);
+        }
+
+        //Getter
+        public string GetDescription()
+        {
+            return _description;
+        }
+
+        //Setter
+        public void SetDescription(string newDescription)
+        {
+            _description = newDescription;
+        }
+
+        public static List<Item> GetAll()
+        {
+            return _instances;
+        }
+
+        public static void ClearAll()
+        {
+            _instances.Clear();
+        }
     }
 }
